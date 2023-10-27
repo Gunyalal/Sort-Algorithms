7ae3f728b46ce79440baa61f9ec6551b0cba8cac
Gunyalal <theslenderhorrors@gmail.com>
2023-10-27 14:35:42 +0400

    Initial commit

---
+++ BubbleSort/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace BubbleSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Randomize = new System.Windows.Forms.Button();
+            this.bubbleSortButton = new System.Windows.Forms.Button();
+            this.selectionSortButton = new System.Windows.Forms.Button();
+            this.mergeSortButton = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.quickSortButton = new System.Windows.Forms.Button();
+            this.insertionSortButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(776, 426);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Randomize
+            // 
+            this.Randomize.Location = new System.Drawing.Point(794, 13);
+            this.Randomize.Name = "Randomize";
+            this.Randomize.Size = new System.Drawing.Size(115, 23);
+            this.Randomize.TabIndex = 1;
+            this.Randomize.Text = "Randomize";
+            this.Randomize.UseVisualStyleBackColor = true;
+            this.Randomize.Click += new System.EventHandler(this.Randomize_Click);
+            // 
+            // bubbleSortButton
+            // 
+            this.bubbleSortButton.Location = new System.Drawing.Point(795, 42);
+            this.bubbleSortButton.Name = "bubbleSortButton";
+            this.bubbleSortButton.Size = new System.Drawing.Size(115, 23);
+            this.bubbleSortButton.TabIndex = 2;
+            this.bubbleSortButton.Text = "Bubble Sort";
+            this.bubbleSortButton.UseVisualStyleBackColor = true;
+            this.bubbleSortButton.Click += new System.EventHandler(this.bubbleSortButton_Click);
+            // 
+            // selectionSortButton
+            // 
+            this.selectionSortButton.Location = new System.Drawing.Point(795, 71);
+            this.selectionSortButton.Name = "selectionSortButton";
+            this.selectionSortButton.Size = new System.Drawing.Size(115, 23);
+            this.selectionSortButton.TabIndex = 3;
+            this.selectionSortButton.Text = "Selection Sort";
+            this.selectionSortButton.UseVisualStyleBackColor = true;
+            this.selectionSortButton.Click += new System.EventHandler(this.selectionSortButton_Click);
+            // 
+            // mergeSortButton
+            // 
+            this.mergeSortButton.Location = new System.Drawing.Point(795, 100);
+            this.mergeSortButton.Name = "mergeSortButton";
+            this.mergeSortButton.Size = new System.Drawing.Size(114, 23);
+            this.mergeSortButton.TabIndex = 4;
+            this.mergeSortButton.Text = "Merge Sort";
+            this.mergeSortButton.UseVisualStyleBackColor = true;
+            this.mergeSortButton.Click += new System.EventHandler(this.mergeSortButton_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(795, 288);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(114, 150);
+            this.Output.TabIndex = 5;
+            this.Output.Text = "";
+            // 
+            // quickSortButton
+            // 
+            this.quickSortButton.Location = new System.Drawing.Point(794, 129);
+            this.quickSortButton.Name = "quickSortButton";
+            this.quickSortButton.Size = new System.Drawing.Size(114, 23);
+            this.quickSortButton.TabIndex = 6;
+            this.quickSortButton.Text = "Quick Sort";
+            this.quickSortButton.UseVisualStyleBackColor = true;
+            this.quickSortButton.Click += new System.EventHandler(this.quickSortButton_Click);
+            // 
+            // insertionSortButton
+            // 
+            this.insertionSortButton.Location = new System.Drawing.Point(794, 158);
+            this.insertionSortButton.Name = "insertionSortButton";
+            this.insertionSortButton.Size = new System.Drawing.Size(114, 23);
+            this.insertionSortButton.TabIndex = 7;
+            this.insertionSortButton.Text = "Insertion Sort";
+            this.insertionSortButton.UseVisualStyleBackColor = true;
+            this.insertionSortButton.Click += new System.EventHandler(this.insertionSortButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 450);
+            this.Controls.Add(this.insertionSortButton);
+            this.Controls.Add(this.quickSortButton);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.mergeSortButton);
+            this.Controls.Add(this.selectionSortButton);
+            this.Controls.Add(this.bubbleSortButton);
+            this.Controls.Add(this.Randomize);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button Randomize;
+        private System.Windows.Forms.Button bubbleSortButton;
+        private System.Windows.Forms.Button selectionSortButton;
+        private System.Windows.Forms.Button mergeSortButton;
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.Button quickSortButton;
+        private System.Windows.Forms.Button insertionSortButton;
+    }
+}
+
---
+++ BubbleSort/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace BubbleSort
+{
+    public partial class Form1 : Form
+    {
+        public void wait(int milliseconds)
+        {
+            var timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+
+          
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+               
+            };
+
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static class Globals
+        {
+          public static int n = 100;
+          public static int[] data = new int[n];
+        }
+        
+        public void randomize()
+        {
+            var rand = new Random();
+            //int[] data = new int[Globals.n];
+            chart1.Series["random array"].Points.Clear();
+
+            for (int i = 0; i < Globals.n; i++)
+            {
+                Globals.data[i] = rand.Next(0, 100);
+                chart1.Series["random array"].Points.AddXY(i + 1, Globals.data[i]);
+                wait(1);
+                //updateChart();
+
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int n = Globals.n;
+            var rand = new Random();
+            int[] data = new int[Globals.n];
+            chart1.Series.Clear();
+            chart1.Series.Add("random array");
+            randomize();
+        }
+        
+        private void updateChart()
+        {
+            for (int i =0;i<Globals.n;i++)
+            {
+                chart1.Series["random array"].Points[i].YValues[0] = Globals.data[i];
+            }
+            chart1.Invalidate();
+        }
+
+        private void bubbleSort()
+        {
+            int n = Globals.n;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (Globals.data[j] > Globals.data[j + 1])
+                    {
+                        // swap temp and arr[i]
+                        int temp = Globals.data[j];
+                        Globals.data[j] = Globals.data[j + 1];
+                        Globals.data[j + 1] = temp;
+                        wait(1);
+                        updateChart();
+
+                    }
+                   
+                    //System.Threading.Thread.Sleep(1);
+                    
+                }
+
+            }
+            
+        }
+
+        private void selectionSort()
+        {
+            int n = Globals.n;
+
+            for(int i = 0;i<n-1;i++)
+            {
+                int min_index = i;
+               // int j = i + 1;
+                for(int j = i+1;j<n;j++)
+                {
+                    if(Globals.data[j]<Globals.data[min_index])
+                    {
+                        min_index = j;
+                        wait(1);
+                        updateChart();
+                    }
+                }
+                int tempInt = Globals.data[min_index];
+                Globals.data[min_index] = Globals.data[i];
+                Globals.data[i] = tempInt;
+
+               // updateChart();
+                     
+            }
+            updateChart();
+        }
+
+        private void merge(int[] Arr, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            int i, j;
+
+            for(i = 0;i<n1;i++)
+            {
+                L[i] = Arr[l + i];
+            }
+            
+            for(j = 0;j<n2;j++)
+            {
+                R[j] = Arr[m+j+1];
+            }
+            i = 0;
+            j = 0;
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    Arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    Arr[k] = R[j];
+                    j++;
+                }
+                k++;
+                wait(1);
+                updateChart();
+            }
+            while (i < n1)
+            {
+                Arr[k] = L[i];
+                i++;
+                k++;
+            }
+            // Copy remaining elements
+            // of R[] if any
+            while (j < n2)
+            {
+                Arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+        void mergeSort(int[] Arr, int l, int r)
+        {
+            if (l < r)
+            {
+                // Find the middle
+                // point
+                int m = l + (r - l) / 2;
+
+                // Sort first and
+                // second halves
+                mergeSort(Arr, l, m);
+                mergeSort(Arr, m + 1, r);
+
+                // Merge the sorted halves
+                merge(Arr, l, m, r);
+
+
+            }
+        }
+
+        void insertionSort()
+        {
+            int n = Globals.n;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = Globals.data[i];
+                int j = i - 1;
+
+                // Move elements of arr[0..i-1],
+                // that are greater than key,
+                // to one position ahead of
+                // their current position
+                while (j >= 0 && Globals.data[j] > key)
+                {
+                    Globals.data[j + 1] = Globals.data[j];
+                    j = j - 1;
+                }
+                Globals.data[j + 1] = key;
+                wait(1);
+                updateChart();
+            }
+        }
+
+        static void swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+
+        int partition(int[] arr, int low, int high)
+        {
+
+
+            int pivot = arr[high];
+
+
+            int i = (low - 1);
+
+            for (int j = low; j <= high - 1; j++)
+            {
+
+
+                if (arr[j] < pivot)
+                {
+
+
+                    i++;
+                    swap(arr, i, j);
+                }
+            }
+            swap(arr, i + 1, high);
+            wait(1);
+            updateChart();
+            return (i + 1);
+            
+        }
+
+
+        void quickSort(int[] arr, int low, int high)
+        {
+            if (low < high)
+            {
+
+
+                int pi = partition(arr, low, high);
+
+                quickSort(arr, low, pi - 1);
+                quickSort(arr, pi + 1, high);
+                updateChart();
+            }
+        }
+
+        private void Randomize_Click(object sender, EventArgs e)
+        {
+            randomize();
+        }
+
+        private void bubbleSortButton_Click(object sender, EventArgs e)
+        {
+            bubbleSort();
+        }
+
+        private void selectionSortButton_Click(object sender, EventArgs e)
+        {
+            selectionSort();
+        }
+
+        private void mergeSortButton_Click(object sender, EventArgs e)
+        {
+            mergeSort(Globals.data, 0, Globals.n - 1);
+            //mergeSort();
+            updateChart();
+
+        }
+
+        private void insertionSortButton_Click(object sender, EventArgs e)
+        {
+            insertionSort();
+            updateChart();
+
+        }
+
+        private void quickSortButton_Click(object sender, EventArgs e)
+        {
+            quickSort(Globals.data, 0, Globals.n - 1);
+            wait(1);
+            updateChart();
+        }
+    }
+}
